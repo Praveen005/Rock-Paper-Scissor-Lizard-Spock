1b000aa92eed714b41808ade40d260d17b113996
Praveen005 <praveen08h@gmail.com>
2023-09-27 13:55:28 +0530

    Refactored the optionsClicked function

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const paper= document.querySelector(".paper");
 const scissor= document.querySelector(".scissor");
 const lizard= document.querySelector(".lizard");
 const spock= document.querySelector(".spock");
-const options= document.querySelectorAll(".options")
+const options= document.querySelectorAll(".options, .rock, .paper, .scissor, .lizard, .spock");
 
 //result area
 const playerScore= document.getElementById("playerScore");
@@ -33,59 +33,36 @@ let classList2= '';
 
 //Player 1 having its pick.
 
-/*
+
 function optionsClicked(event){
-    const pickedOption = event.target.innerHTML;
+    const pickedOption = event.target.outerHTML;
     display1.innerHTML = pickedOption;
+    classList1= Array.from(event.target.classList);
 }
 
 options.forEach(option =>{
     option.addEventListener('click', optionsClicked);
 })
-*/
-
-/*
-The Array.from() method in JavaScript is used to create a new array instance from an array-like or iterable object. It allows you to convert objects that resemble arrays (e.g., strings, NodeList, Set, Map, etc.) into actual arrays.
-
-*/
-
-//event: click event
-//.matches() matches, if the selected element contains that particular selector. :https://developer.mozilla.org/en-US/docs/Web/API/Element/matches
-/*
-why below function was needed, if above was working? 
-: There was an issue with the above code, when clicked on icon inside, the click event was not getting triggered, only the area arounf the icon inside the div upon being clicked was triggering the click event.
-
-what are we doing in the function below?
-: we have passed selector argument as well, it is actually the selector of the outermost parent i.e `.options` here.
-
-`document.documentElement` is the root element of any parent element, say, suppose there is a div tree, 'documentElement' points to the leaf div.
-
-so we started matching from there, then moved up to its parent and so on..
-so, we are checking if the element on which click event happened is present inside the outermost parent or not.
-
-*/
-//This is an overKill though, coud have done the following instead:
-// const options= document.querySelectorAll(".options .fa-solid")
 
 
-function optionsClicked2(event, selector){
-    let currentElement= event.target;
-    while(currentElement !== document.documentElement){
-        if(currentElement.matches(selector)){
-            classList1 = Array.from(currentElement.classList);
+// function optionsClicked2(event, selector){
+//     let currentElement= event.target;
+//     while(currentElement !== document.documentElement){
+//         if(currentElement.matches(selector)){
+//             classList1 = Array.from(currentElement.classList);
             
-            display1.innerHTML= currentElement.innerHTML;
-        }
-        currentElement= currentElement.parentElement;
-    }
+//             display1.innerHTML= currentElement.innerHTML;
+//         }
+//         currentElement= currentElement.parentElement;
+//     }
 
-}
+// }
 
-options.forEach(option =>{
-    option.addEventListener('click', (event)=> {
-        optionsClicked2(event, ".options");
-    })
-})
+// options.forEach(option =>{
+//     option.addEventListener('click', (event)=> {
+//         optionsClicked2(event, ".options");
+//     })
+// })
 
 
 //Bot getting its pick
@@ -94,12 +71,12 @@ function botPick(event){
     const index= Math.floor(Math.random()*5);
     display2.innerHTML= optionsAvailable[index];
     classList2 = Array.from(options[index].classList);
-
 }
 
 options.forEach(option =>{
     option.addEventListener('click', botPick);    
 })
+
 
 
 //Rule for who has win over whom
@@ -127,21 +104,18 @@ function calculateScore(){
     const selectedElement2= classList2[1];
 
     if(selectedElement1 === selectedElement2){
-        console.log("Draw!");
         gameResult.innerText= "It's a Draw!"
     }
-    else if(rule[selectedElement1].includes(selectedElement2)){
-        score1++;
-        console.log("Player 1 won: score1:- ", score1);
-        playerScore.innerText= score1;
-        gameResult.innerText= "Player-1 Won!"
+    else if(rule[selectedElement2].includes(selectedElement1)){
+        score2++;
+        botScore.innerText= score2;
+        gameResult.innerText= "Bot Won!"
 
     }
     else{
-        score2++;
-        console.log("Player 2 won: score2:- ", score2);
-        botScore.innerText= score2;
-        gameResult.innerText= "Bot Won!"
+        score1++;
+        playerScore.innerText= score1;
+        gameResult.innerText= "Player-1 Won!"
     }
 }
 
